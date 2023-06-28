1a0eebdc50f11915c68bf851f1c984306cb69f52
Rajiv <rajiv.jnct@gmail.com>
2023-06-28 16:07:04 +0530

    Created Homecontainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import HomeContainers from './containers/HomeContainers';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <HomeContainers />
     </div>
   );
 }
